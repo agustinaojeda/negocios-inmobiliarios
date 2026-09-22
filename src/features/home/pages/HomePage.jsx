b4d7c446f1b22451a0d8878b88c2e1a6079cc4c4
lucasnahuel1036 <lucasaguirre1036@gmail.com>
2026-09-22 18:26:31 -0300

    Grilla de propiedades y home actualizados

--- src/features/home/pages/HomePage.jsx
+++ src/features/home/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from "../components/Hero";
+import FeaturedGrid from "../components/FeaturedGrid";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <FeaturedGrid />
       
     </main>
   );
